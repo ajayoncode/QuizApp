828e41f6a4dbcf0ee776eafb0d3062c46490cb92
Ajay <ajayoncode@gmail.com>
2021-01-23 18:18:01 +0530

    Extra files removed

--- AwesomeProject/MyComponent/other/FetchLocation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text, View,Button} from 'react-native';
-
-    const FetchLocation = props => {
-        return(
-            <Button title="Get Location" onPress={props.ongetLocation} />
-        );
-    }
-    export default FetchLocation;
